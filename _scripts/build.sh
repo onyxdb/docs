6e4c5dc317d226a5a5d1afee61c585661605bf1f
foxleren <foxleren@gmail.com>
2025-05-24 12:01:14 +0300

    Try web-app-manifest-512x512.png

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,2 +1,2 @@
 yfm --input ./docs --output ./docs-html
-python3 ./_scripts/add_favicon.py ./docs-html ./_assets/web-app-manifest-192x192.png
+python3 ./_scripts/add_favicon.py ./docs-html ./_assets/web-app-manifest-512x512.png
