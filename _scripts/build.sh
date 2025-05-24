c71c68cbbae11a6047cdbc435045444d6a5e7235
foxleren <foxleren@gmail.com>
2025-05-24 12:32:02 +0300

    Remove logo from header

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,2 +1,2 @@
 yfm --input ./docs --output ./docs-html
-python3 ./_scripts/add_favicon.py ./docs-html ./_assets/web-app-manifest-512x512.png
+python3 ./_scripts/add_favicon.py ./docs-html ./_assets/onyxdb_wings_logo_rounded_compressed.png
